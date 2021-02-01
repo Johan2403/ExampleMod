fae6a87038df6fdcb65bf32a3b94b91a764189b5
Johan2403 <johanemman243@gmail.com>
2021-02-01 15:27:16 +0530

    Fix crate rendering issue.

--- src/main/java/mods/johan24/examplemod/block/ObsidianiteCrateBlock.java
+++ src/main/java/mods/johan24/examplemod/block/ObsidianiteCrateBlock.java
@@ -1,10 +1,7 @@
 package mods.johan24.examplemod.block;
 
 import mods.johan24.examplemod.tileentity.ObsidianiteCrateTileEntity;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ContainerBlock;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.piglin.PiglinTasks;
@@ -12,20 +9,25 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.inventory.container.Container;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.stats.Stats;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 import javax.annotation.Nullable;
 
 public class ObsidianiteCrateBlock extends ContainerBlock {
+    public static final DirectionProperty PROPERTY_FACING = BlockStateProperties.FACING;
 
     public ObsidianiteCrateBlock() {
         super(Block.Properties.create(Material.IRON)
@@ -34,6 +36,7 @@ public class ObsidianiteCrateBlock extends ContainerBlock {
                               .harvestLevel(2)
                               .sound(SoundType.METAL)
                               .setRequiresTool());
+        this.setDefaultState(this.getStateContainer().getBaseState().with(PROPERTY_FACING, Direction.NORTH));
     }
 
     @SuppressWarnings("deprecation")
@@ -71,6 +74,12 @@ public class ObsidianiteCrateBlock extends ContainerBlock {
         return new ObsidianiteCrateTileEntity();
     }
 
+    @SuppressWarnings("deprecation")
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
     @Override
     public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
         if(stack.hasDisplayName()) {
@@ -91,5 +100,27 @@ public class ObsidianiteCrateBlock extends ContainerBlock {
     @Override
     public int getComparatorInputOverride(BlockState blockState, World worldIn, BlockPos pos) {
         return Container.calcRedstone(worldIn.getTileEntity(pos));
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, IWorld world, BlockPos pos, Rotation direction) {
+        return state.with(PROPERTY_FACING, direction.rotate(state.get(PROPERTY_FACING)));
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.toRotation(state.get(PROPERTY_FACING)));
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(PROPERTY_FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(PROPERTY_FACING, context.getNearestLookingDirection().getOpposite());
     }
 }
